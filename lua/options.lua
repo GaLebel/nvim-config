17ddd143f71436f6959157d9a97ef47c1766248b
Gal Lebel <gal.lebel@stud.uni-heidelberg.de>
2025-08-05 09:15:46 +0300

    changed tabstop, text-wrap and some other options

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,16 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 2
+vim.opt.sidescrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- vim.opt.softtabstop = 5
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
