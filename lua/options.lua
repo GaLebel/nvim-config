f0173ee417a50db4997b33bf88c7ff3942369a1b
Gal Lebel <gal.lebel@stud.uni-heidelberg.de>
2025-08-05 09:44:38 +0300

    more options, folke/flash setup correctly

--- lua/options.lua
+++ lua/options.lua
@@ -13,7 +13,7 @@ vim.opt.sidescrolloff = 8
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
--- vim.opt.softtabstop = 5
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
